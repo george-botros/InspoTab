24fc38cfcffb451e14695b89c7b405e07f0c6759
Patrick Botros <patrick.botros@duke.edu>
2021-08-27 05:17:05 -0400

    Scaled down images, removed unnecessary background script, and added extension description.

--- Shared (Extension)/Resources/script.js
+++ Shared (Extension)/Resources/script.js
@@ -1,367 +1,378 @@
 let wallpaper1 = {
-  term: "Hope",
-  caption: "The first stop on the road to disappointment.",
-  theme: "rgb(0,0,0)",
-  bgColor: "rgba(0, 0, 0, .5)",
-  image: "disappointment.jpg",
-  gifUrl: "butterfly.gif"
+term: "Hope",
+caption: "The first stop on the road to disappointment.",
+theme: "rgb(0,0,0)",
+bgColor: "rgba(0, 0, 0, .5)",
+image: "disappointment.jpg",
+gifUrl: "butterfly.gif"
 }
 
 let wallpaper2 = {
-  term: "Limitations",
-  caption: "Until you spread your wings you'll never know how far you can walk",
-  gifUrl: "penguin.gif",
-  image: "penguin.jpg",
-  bgColor: "rgba(0, 1, 3, 0.3)",
-  theme: "rgb(80,85,92)"
+term: "Limitations",
+caption: "Until you spread your wings you'll never know how far you can walk",
+gifUrl: "penguin.gif",
+image: "penguin.jpg",
+bgColor: "rgba(0, 1, 3, 0.3)",
+theme: "rgb(80,85,92)"
 }
 
 let wallpaper3 = {
-  term: "Motivation",
-  caption: "Every corpse on Mt. Everest was once a highly motivated individual",
-  gifUrl: "snow.gif", // snow
-  image: "peak.jpg",
-  theme: "rgb(100, 130, 150)",
-  bgColor: "rgba(44, 51, 57, 0.5)"
+term: "Motivation",
+caption: "Every corpse on Mt. Everest was once a highly motivated individual",
+gifUrl: "snow.gif", // snow
+image: "peak.jpg",
+theme: "rgb(100, 130, 150)",
+bgColor: "rgba(44, 51, 57, 0.5)"
 }
 
 let wallpaper4 = {
-  term: "Manifestation",
-  caption: "Every morning wake up and scream your dreams into the garbage can where they belong.",
-  gifUrl: "candyMountain.gif",
-  image: "manifestation.jpg",
-  blur: 2,
-  bgColor: "rgba(0,0,0,.4)",
-  theme: "rgb(9,19,30)"
+term: "Manifestation",
+caption: "Every morning wake up and scream your dreams into the garbage can where they belong.",
+gifUrl: "candyMountain.gif",
+image: "manifestation.jpg",
+blur: 2,
+bgColor: "rgba(0,0,0,.4)",
+theme: "rgb(9,19,30)"
 }
 
 let wallpaper5 = {
-  term: "Humility",
-  caption: "No matter how good you get, there will always be an Asian kid better than you.",
-  gifUrl: "lofiMountain.gif",
-  image: "humility.jpg",
-  bgColor: "rgba(44, 51, 57, 0.5)",
-  theme: "rgb(87,115,127)"
+term: "Humility",
+caption: "No matter how good you get, there will always be an Asian kid better than you.",
+gifUrl: "lofiMountain.gif",
+image: "humility.jpg",
+bgColor: "rgba(44, 51, 57, 0.5)",
+theme: "rgb(87,115,127)"
 }
 
 let wallpaper6 = {
-  term: "Confidence",
-  caption: "You can do it. But you probably won't.",
-  gifUrl: "grayWave.gif",
-  image: "city.jpg",
-  blur: 2,
-  bgColor: "rgba(0, 0, 0, 0.2)",
-  theme: "rgb(76,65,88)"
+term: "Confidence",
+caption: "You can do it. But you probably won't.",
+gifUrl: "grayWave.gif",
+image: "city.jpg",
+blur: 2,
+bgColor: "rgba(0, 0, 0, 0.2)",
+theme: "rgb(76,65,88)"
 }
 
 let wallpaper7 = {
-  term: "Mistakes",
-  caption: "It could be that the purpose of your life is to serve as a warning for others.",
-  gifUrl: "mug.gif", // mug
-  image: "mistake.jpg",
-  blur: 2,
-  bgColor: "rgba(0, 0, 0, 0.7)",
-  theme: "rgb(18,45,45)"
+term: "Mistakes",
+caption: "It could be that the purpose of your life is to serve as a warning for others.",
+gifUrl: "mug.gif", // mug
+image: "mistake.jpg",
+blur: 2,
+bgColor: "rgba(0, 0, 0, 0.7)",
+theme: "rgb(18,45,45)"
 }
 
 
 let wallpaper8 = {
-  term: "Inspiration",
-  caption: "You just need that one bright idea to shine a light on all the time you wasted.",
-  gifUrl: "liftoff.gif",
-  image: "idea.jpg",
-  blur: 0,
-  bgColor: "rgba(0, 0, 0, 0.5)",
-  theme: "rgb(2,2,2)"
+term: "Inspiration",
+caption: "You just need that one bright idea to shine a light on all the time you wasted.",
+gifUrl: "liftoff.gif",
+image: "idea.jpg",
+blur: 0,
+bgColor: "rgba(0, 0, 0, 0.5)",
+theme: "rgb(2,2,2)"
 }
 
 let wallpaper9 = {
-  term: "Imagination",
-  caption: "Picture it: Somewhere under the waves a magestic seal is being torn in half by a shark.",
-  gifUrl: "rainbowDrop.gif",
-  image: "seal.jpg",
-  blur: 2,
-  bgColor: "rgba(0, 0, 0, 0.6)",
-  theme: "rgb(79,70,58)"
+term: "Imagination",
+caption: "Picture it: Somewhere under the waves a magestic seal is being torn in half by a shark.",
+gifUrl: "rainbowDrop.gif",
+image: "seal.jpg",
+blur: 2,
+bgColor: "rgba(0, 0, 0, 0.6)",
+theme: "rgb(79,70,58)"
 }
 
 let wallpaper10 = {
-  term: "The Future",
-  caption: "Similar to the past except you die in it.",
-  gifUrl: "abstractBlue.gif",
-  image: "success.jpg",
-  blur: 6,
-  bgColor: "rgba(0, 0, 0, 0.5)",
-  theme: "rgb(52,52,85)"
+term: "The Future",
+caption: "Similar to the past except you die in it.",
+gifUrl: "abstractBlue.gif",
+image: "success.jpg",
+blur: 6,
+bgColor: "rgba(0, 0, 0, 0.5)",
+theme: "rgb(52,52,85)"
 }
 
 let wallpaper11 = {
-  term: "Mediocrity",
-  caption: "Just because we've accepted you as you are, doesn't mean we've abandoned hope you'll improve.",
-  gifUrl: "gamecube.gif", // gamecube
-  image: "failure.jpg",
-  bgColor: "rgba(0, 0, 0, 0.6)",
-  theme: "rgb(50,70,80)"
+term: "Mediocrity",
+caption: "Just because we've accepted you as you are, doesn't mean we've abandoned hope you'll improve.",
+gifUrl: "gamecube.gif", // gamecube
+image: "failure.jpg",
+bgColor: "rgba(0, 0, 0, 0.6)",
+theme: "rgb(50,70,80)"
 }
 
 let wallpaper12 = {
-  term: "Pressure",
-  caption: "It can turn a lump of coal into a flawless diamond and an average person into an absolute basketcase.",
-  gifUrl: "diamond.gif", // diamond
-  image: "diamond.jpg",
-  bgColor: "rgba(0, 0, 0, 0.6)",
-  theme: "rgb(20,30,50)"
+term: "Pressure",
+caption: "It can turn a lump of coal into a flawless diamond and an average person into an absolute basketcase.",
+gifUrl: "diamond.gif", // diamond
+image: "diamond.jpg",
+bgColor: "rgba(0, 0, 0, 0.6)",
+theme: "rgb(20,30,50)"
 }
 
 let wallpaper13 = {
-  term: "Inevitability",
-  caption: "It really doesn't matter if you're good or bad: Ebola may still get you.",
-  gifUrl: "germ.gif", // germ
-  image: "ebola.jpg",
-  bgColor: "rgba(0, 0, 0, 0.6)",
-  theme: "rgb(34,29,71)",
+term: "Inevitability",
+caption: "It really doesn't matter if you're good or bad: Ebola may still get you.",
+gifUrl: "germ.gif", // germ
+image: "ebola.jpg",
+bgColor: "rgba(0, 0, 0, 0.6)",
+theme: "rgb(34,29,71)",
 }
 
 let wallpaper14 = {
-  term: "Acheivement",
-  caption: "The tallest blade of grass is the worst cut.",
-  gifUrl: "jaws.gif", // jaws
-  image: "mower.jpg",
-  bgColor: "rgba(0, 0, 0, 0.6)",
-  theme: "rgb(23,38,29)"
+term: "Acheivement",
+caption: "The tallest blade of grass is the worst cut.",
+gifUrl: "jaws.gif", // jaws
+image: "mower.jpg",
+bgColor: "rgba(0, 0, 0, 0.6)",
+theme: "rgb(23,38,29)"
 }
 
 let wallpaper15 = {
-  term: "Procrastinate",
-  caption: "You're going to procrastinate eventually, so you might as well stop putting it off.",
-  gifUrl: "vertical.gif", // vertical
-  image: "procrastinate.jpg",
-  theme: "rgb(15,27,32)"
+term: "Procrastinate",
+caption: "You're going to procrastinate eventually, so you might as well stop putting it off.",
+gifUrl: "vertical.gif", // vertical
+image: "procrastinate.jpg",
+theme: "rgb(15,27,32)"
 }
 
 wallpaper16 = {
-  term: "The Internet",
-  caption: "Where the population is entirely crazy. Welcome home.",
-  gifUrl: "binary.gif",
-  image: "internet.jpg",
-  bgColor: "rgba(0, 0, 0, 0.6)",
-  blur: 1,
-  theme: "rgb(30,160,155)"
+term: "The Internet",
+caption: "Where the population is entirely crazy. Welcome home.",
+gifUrl: "binary.gif",
+image: "internet.jpg",
+bgColor: "rgba(0, 0, 0, 0.6)",
+blur: 1,
+theme: "rgb(30,160,155)"
 }
 
 wallpaper17 = {
-  term: "Procrastination",
-  caption: "I'll find a picture for this tomorrow.",
-  gifUrl: "binary.gif",
-  bgColor: "rgba(0, 0, 0)",
-  theme: "black",
+term: "Procrastination",
+caption: "I'll find a picture for this tomorrow.",
+gifUrl: "binary.gif",
+bgColor: "rgba(0, 0, 0)",
+theme: "black",
 }
 
 wallpaper18 = {
-  term: "Aliens",
-  caption: "They get the job done.",
-  gifUrl: "ufo.gif",
-  image: "slavery.jpg",
-  bgColor: "rgba(0, 0, 0, 0.6)",
-  blur: 1,
-  theme: "rgb(48,62,64)"
+term: "Aliens",
+caption: "They get the job done.",
+gifUrl: "ufo.gif",
+image: "slavery.jpg",
+bgColor: "rgba(0, 0, 0, 0.6)",
+blur: 1,
+theme: "rgb(48,62,64)"
 }
 
 wallpaper19 = {
-  term: "Today",
-  caption: "Is the first day of the rest of your life, but so was yesterday and look how that turned out.",
-  gifUrl: "vertical.gif",
-  image: "brutalism.jpg",
-  bgColor: "rgba(0, 0, 0, 0.3)",
-  theme: "rgb(120, 113, 107)"
+term: "Today",
+caption: "Is the first day of the rest of your life, but so was yesterday and look how that turned out.",
+gifUrl: "vertical.gif",
+image: "brutalism.jpg",
+bgColor: "rgba(0, 0, 0, 0.3)",
+theme: "rgb(120, 113, 107)"
 }
 
 wallpaper20 = {
-  term: "Distraction",
-  caption: "While you were looking at a bust, ninjas stole all the beer in your fridge.",
-  gifUrl: "yellow.gif",
-  image: "distraction.jpg",
-  theme: "rgb(83,68,21)",
-  bgColor: "rgba(0, 0, 0, 0.3)"
+term: "Distraction",
+caption: "While you were looking at a bust, ninjas stole all the beer in your fridge.",
+gifUrl: "yellow.gif",
+image: "distraction.jpg",
+theme: "rgb(83,68,21)",
+bgColor: "rgba(0, 0, 0, 0.3)"
 }
 
 wallpaper21 = {
-  term: "Anarchists",
-  caption: "Proving we don't need a government by reminding us we need one.",
-  gifUrl: "anarchy.gif",
-  image: "anarchy.jpg",
-  theme: "rgb(153, 41, 41)",
-  bgColor: "rgba(0, 0, 0, 0.2)"
+term: "Anarchists",
+caption: "Proving we don't need a government by reminding us we need one.",
+gifUrl: "anarchy.gif",
+image: "anarchy.jpg",
+theme: "rgb(153, 41, 41)",
+bgColor: "rgba(0, 0, 0, 0.2)"
 }
 
 wallpaper22 = {
-  term: "Carpe Diem",
-  caption: "There's no better day than today to regret yesterday and dread tomorrow.",
-  gifUrl: "octopusAbstract.gif",
-  image: "carpe.jpg",
-  bgColor: "rgba(0, 0, 0, 0.7)",
-  theme: "rgb(42,37,35)",
-  blur: 5
+term: "Carpe Diem",
+caption: "There's no better day than today to regret yesterday and dread tomorrow.",
+gifUrl: "octopusAbstract.gif",
+image: "carpe.jpg",
+bgColor: "rgba(0, 0, 0, 0.7)",
+theme: "rgb(42,37,35)",
+blur: 5
 }
 
 wallpaper23 = {
-  term: "Meanwhile",
-  caption: "In Hell",
-  gifUrl: "fire.gif", // fire
-  image: "bieber.jpg",
-  bgColor: "rgba(0, 0, 0, 0.7)",
-  blur: 3,
-  theme: "rgb(16,24,34)"
+term: "Meanwhile",
+caption: "In Hell",
+gifUrl: "fire.gif", // fire
+image: "bieber.jpg",
+bgColor: "rgba(0, 0, 0, 0.7)",
+blur: 3,
+theme: "rgb(16,24,34)"
 }
 
 wallpaper24 = {
-  term: "Angry Birds",
-  caption: "Beta",
-  gifUrl: "angryBirds.gif", // angry birds
-  image: "bird.jpg",
-  bgColor: "rgba(0, 0, 0, 0.5)",
-  theme: "rgb(43,43,35)"
+term: "Angry Birds",
+caption: "Beta",
+gifUrl: "angryBirds.gif", // angry birds
+image: "bird.jpg",
+bgColor: "rgba(0, 0, 0, 0.5)",
+theme: "rgb(43,43,35)"
 }
 
 wallpaper25 = {
-  term: "Give up",
-  caption: "At some point, hanging in there just makes you look like an even bigger loser.",
-  gifUrl: "blackDot.gif", // blackDot
-  image: "raindrops.jpg",
-  bgColor: "rgba(0, 0, 0, 0.7)",
-  theme: "rgb(42,53,54)"
+term: "Give up",
+caption: "At some point, hanging in there just makes you look like an even bigger loser.",
+gifUrl: "blackDot.gif", // blackDot
+image: "raindrops.jpg",
+bgColor: "rgba(0, 0, 0, 0.7)",
+theme: "rgb(42,53,54)"
 }
 
 wallpaper26 = {
-  term: "Failure Is Not An Option",
-  caption: "It is your destiny.",
-  gifUrl: "test.gif", // test
-  image: "spacex.jpg",
-  bgColor: "rgba(0, 0, 0, 0.7)",
-  theme: "rgb(23,27,32)"
+term: "Failure Is Not An Option",
+caption: "It is your destiny.",
+gifUrl: "test.gif", // test
+image: "spacex.jpg",
+bgColor: "rgba(0, 0, 0, 0.7)",
+theme: "rgb(23,27,32)"
 }
 
 wallpaper27 = {
-  term: "Progress",
-  caption: "Just makes you irrelevant.",
-  gifUrl: "lightspeed.gif", // lightspeed
-  image: "progress.jpg",
-  bgColor: "rgba(30, 30, 30, 0.8)",
-  theme: "rgb(24,40,40)"
+term: "Progress",
+caption: "Just makes you irrelevant.",
+gifUrl: "lightspeed.gif", // lightspeed
+image: "progress.jpg",
+bgColor: "rgba(30, 30, 30, 0.8)",
+theme: "rgb(24,40,40)"
 }
 
 wallpaper28 = {
-  term: "Bailouts",
-  caption: "From each according to his ability, to each according to his lack thereof.",
-  gifUrl: "money.gif", // money
-  image: "wallst.jpg",
-  bgColor: "rgba(0, 0, 0, 0.65)",
-  theme: "rgb(140,180,108)"
+term: "Bailouts",
+caption: "From each according to his ability, to each according to his lack thereof.",
+gifUrl: "money.gif", // money
+image: "wallst.jpg",
+bgColor: "rgba(0, 0, 0, 0.65)",
+theme: "rgb(140,180,108)"
 }
 
 wallpaper29 = {
-  term: "The sky is the limit",
-  caption: "Too bad you can't fly.",
-  gifUrl: "sky2.gif", // sky
-  image: "ostrich.jpg",
-  bgColor: "rgba(0, 0, 0, 0.3)",
-  theme: "rgb(130,150,155)"
+term: "The sky is the limit",
+caption: "Too bad you can't fly.",
+gifUrl: "sky2.gif", // sky
+image: "ostrich.jpg",
+bgColor: "rgba(0, 0, 0, 0.3)",
+theme: "rgb(130,150,155)"
 }
 
 wallpaper30 = {
-  term: "Face it",
-  caption: "You'll never be as cool as these guys.",
-  gifUrl: "trek.gif", // trek
-  image: "startrek.jpg",
-  bgColor: "rgba(30, 30, 30, .5)",
-  theme: "rgb(45,28,25)"
+term: "Face it",
+caption: "You'll never be this cool.",
+gifUrl: "trek.gif", // trek
+image: "startrek.jpg",
+bgColor: "rgba(30, 30, 30, .5)",
+theme: "rgb(45,28,25)"
 }
 
 wallpaper31 = {
-  term: "Your argument",
-  caption: "has never been more invalid",
-  gifUrl: "skyBreakingBad.gif", // sky Breaking Bad
-  image: "invalid.jpg",
-  bgColor: "rgba(0, 0, 0, .5)",
-  theme: "rgb(99,88,81)"
+term: "Your argument",
+caption: "has never been more invalid",
+gifUrl: "skyBreakingBad.gif", // sky Breaking Bad
+image: "invalid.jpg",
+bgColor: "rgba(0, 0, 0, .5)",
+theme: "rgb(99,88,81)"
 }
 
 wallpaper32 = {
-  term: "Air travel",
-  caption: "You're doing it wrong.",
-  gifUrl: "sky.gif", // sky2
-  image: "flight.jpg",
-  bgColor: "rgba(0, 0, 0, .3)",
-  theme: "rgb(27,38,25)"
+term: "Air travel",
+caption: "You're doing it wrong.",
+gifUrl: "sky.gif", // sky2
+image: "flight.jpg",
+bgColor: "rgba(0, 0, 0, .3)",
+theme: "rgb(27,38,25)"
 }
 
 wallpaper33 = {
-  term: "Tasks",
-  caption: "By doing a little bit each day, you can gradually let the job overwhelm you.",
-  gifUrl: "grayscaleAbstract.gif", // grayscal Abstract
-  image: "ocean.jpg",
-  blur: 2,
-  theme: "black"
+term: "Tasks",
+caption: "By doing a little bit each day, you can gradually let the job overwhelm you.",
+gifUrl: "grayscaleAbstract.gif", // grayscal Abstract
+image: "ocean.jpg",
+blur: 2,
+theme: "black"
 }
 
 var wallpapers = [wallpaper1, wallpaper2, wallpaper3, wallpaper4, wallpaper5, wallpaper6, wallpaper7, wallpaper8, wallpaper9, wallpaper10, wallpaper11, wallpaper12, wallpaper13, wallpaper14, wallpaper15, wallpaper16, wallpaper17, wallpaper18, wallpaper19, wallpaper20, wallpaper21, wallpaper22, wallpaper23, wallpaper24, wallpaper25, wallpaper26, wallpaper27, wallpaper28, wallpaper29, wallpaper30, wallpaper31, wallpaper32, wallpaper33]
 
-let i = Math.floor(Math.random() * wallpapers.length)
+var i = parseInt(localStorage.getItem("index"))
+if (i === null) {
+    localStorage.setItem("index", 0)
+    i = 0
+}
+
+console.log(i)
 
 let randomWallpaper = wallpapers[i]
 
+console.log(randomWallpaper)
+
 window.addEventListener('load', function () {
-
-  // set background image.
-  if (randomWallpaper.image && randomWallpaper.blur) {
-    let bgString = "background-image: url('" + browser.extension.getURL(randomWallpaper.image) + "'); filter: blur(" + randomWallpaper.blur + "px)"
-    document.querySelector("#background").setAttribute("style", bgString)
-  } else if (randomWallpaper.image) {
-    let bgString = "background-image: url('" + browser.extension.getURL(randomWallpaper.image) + "');"
-    document.querySelector("#background").setAttribute("style", bgString)
-  }
-
-  // set background color or color overlay.
-  if (randomWallpaper.bgColor) {
-    if (randomWallpaper.image) {
-      let bgString = document.querySelector("#background").getAttribute("style") + "grayscale();"
+    
+    // set background image.
+    if (randomWallpaper.image && randomWallpaper.blur) {
+        let bgString = "background-image: url('" + browser.extension.getURL(randomWallpaper.image) + "'); filter: blur(" + randomWallpaper.blur + "px)"
+        document.querySelector("#background").setAttribute("style", bgString)
+    } else if (randomWallpaper.image) {
+        let bgString = "background-image: url('" + browser.extension.getURL(randomWallpaper.image) + "');"
+        document.querySelector("#background").setAttribute("style", bgString)
     }
-    let bgString = "background-color: " + randomWallpaper.bgColor
-    document.querySelector("#backgroundColorOverlay").setAttribute("style", bgString)
-  }
-
-  // set foreground text to quote and attach relevant text stylesheet.
-  document.querySelector("#gif").innerText = randomWallpaper.term
-  document.querySelector("#caption").innerText = randomWallpaper.caption
-
-  // set title
-  setTitle(randomWallpaper.term)
-
-  // set toolbar "meta" theme color
-  if (randomWallpaper.theme !== null) {
-    setThemeColor(randomWallpaper.theme)
-  }
-
-  // set gif url if applicable.
-  if (randomWallpaper.gifUrl != null) {
-    let gifString = "background-image: url('" + browser.extension.getURL(randomWallpaper.gifUrl) + "');"
-    document.querySelector("#gif").setAttribute("style", gifString)
-  }
+    
+    // set background color or color overlay.
+    if (randomWallpaper.bgColor) {
+        if (randomWallpaper.image) {
+            let bgString = document.querySelector("#background").getAttribute("style") + "grayscale();"
+        }
+        let bgString = "background-color: " + randomWallpaper.bgColor
+        document.querySelector("#backgroundColorOverlay").setAttribute("style", bgString)
+    }
+    
+    // set foreground text to quote and attach relevant text stylesheet.
+    document.querySelector("#gif").innerText = randomWallpaper.term
+    document.querySelector("#caption").innerText = randomWallpaper.caption
+    
+    // set title
+    setTitle(randomWallpaper.term)
+    
+    // set toolbar "meta" theme color
+    if (randomWallpaper.theme !== null) {
+        setThemeColor(randomWallpaper.theme)
+    }
+    
+    // set gif url if applicable.
+    if (randomWallpaper.gifUrl != null) {
+        let gifString = "background-image: url('" + browser.extension.getURL(randomWallpaper.gifUrl) + "');"
+        document.querySelector("#gif").setAttribute("style", gifString)
+    }
+    
+    // update index
+    localStorage.setItem("index", i+1 % wallpapers.length)
 })
 
 function setThemeColor(color) {
-  var head = document.head;
-  var meta = document.createElement("meta");
-
-  meta.name = "theme-color";
-  meta.content = color;
-  head.appendChild(meta)
+    var head = document.head;
+    var meta = document.createElement("meta");
+    
+    meta.name = "theme-color";
+    meta.content = color;
+    head.appendChild(meta)
 }
 
 function setTitle(term) {
-  var head = document.head;
-  var title = document.createElement("title");
-
-  title.innerText = term;
-  head.appendChild(title)
+    var head = document.head;
+    var title = document.createElement("title");
+    
+    title.innerText = term;
+    head.appendChild(title)
 }
